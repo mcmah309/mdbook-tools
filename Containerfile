ed5d1320a84beeaa6690c28b7d2d9ce2321da8b7
mcmah309 <dillonmcmahon79@gmail.com>
2024-02-05 22:25:19 +0000

    refactor: Remove unneeded files

--- Containerfile
+++
@@ -1,7 +0,0 @@
-# Container file used to develop
-
-FROM rust:1.74.1
-WORKDIR /app
-
-# Leave container running until shutdown
-CMD ["tail", "-f", "/dev/null"]
--- shell.nix
+++
@@ -1,9 +0,0 @@
-# Anything non-container related that is needed to develop and not wanted system-wide
-
-{ pkgs ? import <nixpkgs> {}}:
-
-pkgs.mkShell {
-  packages = [ 
-    pkgs.hello
-  ];
-}
